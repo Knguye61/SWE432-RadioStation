70462d0a8668abf287a58ddaeb11995d4563cc45
Knguye61 <78722907+Knguye61@users.noreply.github.com>
2023-11-02 03:07:51 +0000

    stuff

--- RadioStation/server.js
+++ RadioStation/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
+
+app.get('/', function(req, res){
+
+
+})
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
